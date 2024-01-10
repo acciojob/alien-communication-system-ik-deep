7719c26d20e181a796a1b17645538f06fc1ff09c
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-01-10 21:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+    // Simulate microtasks
+    Promise.resolve().then(() => console.log('Microtask 1 executed'));
+    Promise.resolve().then(() => console.log('Microtask 2 executed'));
+
+    // Simulate macrotasks
+    setTimeout(() => console.log('Macrotask 1 executed'), 0);
+    setTimeout(() => console.log('Macrotask 2 executed'), 0);
+
